450cf73f5872f106c016f9661b202e82c4c91570
Joseph M <jmedina1985@hotmail.com>
2021-02-23 22:39:26 -0800

    Working on Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,7 +8,12 @@ function Todo() {
     value: '',
   });
 
-  return todos.map((todo, index) => <div className={}></div>);
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    ></div>
+  ));
 }
 
 export default Todo;
