9220554b7b9a639c3cf02f21a6b8f9068d786e19
Joseph M <jmedina1985@hotmail.com>
2021-02-23 22:38:07 -0800

    Working on Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 function Todo() {
   const [edit, setEdit] = useState({
@@ -7,7 +8,7 @@ function Todo() {
     value: '',
   });
 
-  return <div></div>;
+  return todos.map((todo, index) => <div className={}></div>);
 }
 
 export default Todo;
