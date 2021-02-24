42929ba64382424bc1c1e0369dddddec11952071
Joseph M <jmedina1985@hotmail.com>
2021-02-23 22:41:01 -0800

    Working on Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,7 +13,9 @@ function Todo() {
       className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
       key={index}
     >
-      <div key={todo.id}></div>
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
     </div>
   ));
 }
