86bf9138d785f025405312feb349dab22d81741a
Joseph M <jmedina1985@hotmail.com>
2021-02-23 22:36:49 -0800

    Working on Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 
 function Todo() {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+  });
+
   return <div></div>;
 }
 
