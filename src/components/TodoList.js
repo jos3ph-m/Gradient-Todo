7ef781f2316c6b84018e8bb675d658eb445a7387
Joseph M <jmedina1985@hotmail.com>
2021-02-24 22:06:15 -0800

    updateTodo functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -45,7 +45,12 @@ function TodoList() {
     <div>
       <h1>What's the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
