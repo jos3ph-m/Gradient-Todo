81bc2d47ced8e046a6acba6f4c239b8542ff3f7c
Joseph M <jmedina1985@hotmail.com>
2021-02-26 18:06:14 -0800

    edit full functionality

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
   const inputRef = useRef(null);
 
