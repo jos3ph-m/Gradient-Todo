56bffe663763201441e292c4922e594603aa350f
Joseph M <jmedina1985@hotmail.com>
2021-02-26 22:17:37 -0800

    changing ID building

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -17,7 +17,7 @@ function TodoForm(props) {
     e.preventDefault();
 
     props.onSubmit({
-      id: Math.floor(Math.random() * 80000),
+      id: Math.floor(Math.random() * 85000),
       text: input,
     });
 
